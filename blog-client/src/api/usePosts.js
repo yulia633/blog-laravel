1c18e31ebc1d767f887229dafd5044319d2f0c1c
yulia633 <ignat633yulia@yandex.ru>
2021-11-06 14:46:14 +0300

    add listing posts

---
+++ blog-client/src/api/usePosts.js
@@ -0,0 +1,17 @@
+import { ref } from 'vue'
+import axios from 'axios'
+
+export default function usePosts() {
+    const posts = ref([])
+
+    const fetchPosts = async () => {
+        let response = await axios.get('/api/posts');
+        posts.value = response.data.data
+    }
+
+    return {
+        posts,
+        fetchPosts
+        
+    }
+}
--- blog-client/src/main.js
+++ blog-client/src/main.js
@@ -2,5 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './assets/app.css'
 import router from './router'
+import axios from 'axios'
+
+axios.defaults.baseURL = 'http://localhost:8000/'
 
 createApp(App).use(router).mount('#app')
