1af50bec9e22e7267b4ddc9b714557c64b18d38d
yulia633 <ignat633yulia@yandex.ru>
2021-11-17 16:09:40 +0300

    add renderless textarea height component

--- blog-client/src/components/ResizeTextarea.vue
+++ blog-client/src/components/ResizeTextarea.vue
@@ -1,14 +1,24 @@
 <script>
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
 
 export default {
   setup(props, context) {
-    const greeting = ref("Hello")
-
+    const el = ref(null)
+    const resizeTarget = (target) => {
+        target.style.height = 'auto'
+        target.style.height = `${target.scrollHeight}px`
+    }
+    const resize = (e) => {
+        resizeTarget(e.target)
+    }
+    onMounted(() => {
+        resizeTarget(el.value)
+    })
     return () =>
       context.slots.default({
-        greeting
+        resize,
+        el
       })
   },
-};
+}
 </script>
--- blog-client/src/pages/admin/Edit.vue
+++ blog-client/src/pages/admin/Edit.vue
@@ -1,9 +1,8 @@
 <template>
     <div>
-        <ResizeTextarea>
-            <template v-slot:default="{ greeting }">
-                {{ greeting }}
-                <textarea v-model="post.title" class="w-full text-center text-4xl lg:text-6xl
+        <ResizeTextarea v-if="post.title">
+            <template v-slot:default="{ resize, el }">
+                <textarea :ref="el" v-model="post.title" v-on:input="resize" class="w-full text-center text-4xl lg:text-6xl
             leading-10 font-extrabold tracking-tight text-gray-900 border-none focus:ring-0 resize-none p-0"></textarea>
             </template>
         </ResizeTextarea>
